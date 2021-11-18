adf1a929cf7b5d5d8c53fcde66251b948bb545b6
Mauro <mauromarchesan88@gmail.com>
2021-11-18 15:48:26 -0300

    Adicionando exercicios de HTML CSS e JS

---
+++ Fundamentos/2-Git/Bloco2/minha_pagina.js
@@ -0,0 +1,5 @@
+// Limpar o leitor com um click duplo
+const clearButton = document.querySelector('.limpar');
+clearButton.addEventListener('dblclick', function () {
+  inputResultado.value = "";
+});
