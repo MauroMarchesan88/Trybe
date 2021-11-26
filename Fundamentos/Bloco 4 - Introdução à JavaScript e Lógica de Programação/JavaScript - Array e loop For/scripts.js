b353b88e32bda6de06fa258abb34eb132ebed4f1
Mauro <mauromarchesan88@gmail.com>
2021-11-26 12:58:20 -0300

    Primeiros passos parte 3

--- Fundamentos/Bloco 4 - Introdução à JavaScript e Lógica de Programação/JavaScript - Array e loop For/scripts.js
+++ Fundamentos/Bloco 4 - Introdução à JavaScript e Lógica de Programação/JavaScript - Array e loop For/scripts.js
@@ -1,6 +1,9 @@
-const base = 5;
-const height = 8;
-const area = base * height;
-const perimeter = base *4
+let nota = 70
 
-console.log(base,height,area,perimeter)
+if (nota >= 80) {
+    console.log("Parabéns, você foi aprovada(o)!")
+} else if (nota < 80 && nota >= 60) {
+    console.log("Voce esta na nossa lista de espera")
+}else {
+    console.log("Voce foi reprovada(o)")
+}
