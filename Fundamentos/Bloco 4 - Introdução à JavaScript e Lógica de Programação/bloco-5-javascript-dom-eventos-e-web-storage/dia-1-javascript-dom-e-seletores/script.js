4372421b6d11ae0867126e6bca9e1b0f32158284
Mauro <mauromarchesan88@gmail.com>
2021-12-05 12:25:47 -0300

    terminado exercicio do course

--- Fundamentos/Bloco 4 - Introdução à JavaScript e Lógica de Programação/bloco-5-javascript-dom-eventos-e-web-storage/dia-1-javascript-dom-e-seletores/script.js
+++ Fundamentos/Bloco 4 - Introdução à JavaScript e Lógica de Programação/bloco-5-javascript-dom-eventos-e-web-storage/dia-1-javascript-dom-e-seletores/script.js
@@ -1,7 +1,19 @@
 document.getElementById("header-container").style.backgroundColor = "green";
-document.getElementById("footer-container").style.backgroundColor = "darkGreen"
-let noEmergencyTasks = document.getElementsByClassName("no-emergency-tasks");
+document.getElementById("footer-container").style.backgroundColor = "darkGreen";
+let divs = document.getElementsByTagName('div');
+let emergencyTasks = document.getElementsByTagName('h3');
 
-for (const task of noEmergencyTasks) {
-    noEmergencyTasks[task].style.backgroundColor = "black";
+for (let i = 0; i < divs.length && i <= 3; i += 1) {
+    divs[i].style.backgroundColor = "pink";
+    if (i > 1 && i <= 3) {
+        divs[i].style.backgroundColor = "yellow";
+    }
 }
+
+for (let k = 0; k <= emergencyTasks.length; k += 1) {
+    emergencyTasks[k].style.backgroundColor = "purple";
+    if (k > 1 && k <= 3) {
+        emergencyTasks[k].style.backgroundColor = "black";
+    }
+}
+
