e8b5787d2bd54561cbab0b3d24a7d3d9e7568b75
Mauro <mauromarchesan88@gmail.com>
2022-01-06 20:22:54 -0300

    bootstrap, datepicker funcionando sem erros

---
+++ Fundamentos/bloco-6-html-e-css-forms-flexbox-e-responsivo/dia-2-bibliotecas-javascript-e-frameworks-css/script.js
@@ -0,0 +1,16 @@
+// DatePicjerX call
+document.getElementById('input-dataInicio').DatePickerX.init({format:'dd-mm-yyyy'});
+// PopupValidation setup
+
+
+const estados = document.getElementById('estado');
+const arrayEstados = ['Acre', 'Alagoas', 'Amapá', 'Amazonas', 'Bahia', 'Ceará', 'Espirito Santo', 'Goias', 'Maranhao', 'Mato Grosso', 'Mato Grosso do Sul', 'Minas Gerais', 'Para', 'Paraiba', 'Parana', 'Pernambuco', 'Piaui', 'Rio de Janeiro', 'Rio Grande do Norte', 'Rio Grande do Sul', 'Rondonia', 'Roraima', 'Santa Catarina', 'Sao Paulo', 'Sergipe', 'Tocantins', 'Distrito Federal'];
+for (let i = 0; i < arrayEstados.length; i += 1) {
+    let opcion = document.createElement('option');
+    opcion.innerText = arrayEstados[i];
+    estados.appendChild(opcion);
+}
+const inputs = document.getElementsByTagName('input');
+const footer = document.getElementById('footer');
+const botaoLimpar = document.getElementById('limpar');
+validation.init('name')
