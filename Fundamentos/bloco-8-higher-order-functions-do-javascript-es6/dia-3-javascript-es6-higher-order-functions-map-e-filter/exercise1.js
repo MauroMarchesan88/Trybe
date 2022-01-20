f80bca239ac3adbd78185b5863a7fac4ef15e92a
Mauro <mauromarchesan88@gmail.com>
2022-01-20 16:16:10 -0300

    exercicio 1 completo

--- Fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-3-javascript-es6-higher-order-functions-map-e-filter/exercise1.js
+++ Fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-3-javascript-es6-higher-order-functions-map-e-filter/exercise1.js
@@ -62,3 +62,19 @@ const books = [
   ];
   
   // Adicione o código do exercício aqui:
+  const expectedResult = [
+    'As Crônicas de Gelo e Fogo - Fantasia - George R. R. Martin',
+    'O Senhor dos Anéis - Fantasia - J. R. R. Tolkien',
+    'Fundação - Ficção Científica - Isaac Asimov',
+    'Duna - Ficção Científica - Frank Herbert',
+    'A Coisa - Terror - Stephen King',
+    'O Chamado de Cthulhu - Terror - H. P. Lovecraft',
+  ];
+  
+  function formatedBookNames() {
+    // escreva seu código aqui
+    const newList = books.map((book) => `${book.name} ${book.genre} ${book.author.name}`);
+    return newList;
+  }
+
+  console.log(formatedBookNames(books));
