c2d412fd74c400c1f033e85b04917204d0d75ed2
MauroMarchesan88 <mauromarchesan88@gmail.com>
2022-02-17 15:19:48 -0300

    tentando nao quebrar tudo com o node modules e gitignore ignorado

---
+++ Front-End/bloco-10-introducao-a-react/dia-1-hello-world-no-react/ex3/src/App.js
@@ -0,0 +1,15 @@
+import Header from './Header';
+import './App.css';
+import Content from './Content';
+import Footer from './Footer';
+
+function App() {
+  return (<div>
+  <Header />
+  <Content />
+  <Footer classname='footer' />
+  </div>
+  )
+}
+
+export default App;
