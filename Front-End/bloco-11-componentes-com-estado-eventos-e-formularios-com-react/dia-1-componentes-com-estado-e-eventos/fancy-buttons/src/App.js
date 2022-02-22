32da04cd63d7812289fb3cea0d691bc464393acf
MauroMarchesan88 <mauromarchesan88@gmail.com>
2022-02-22 11:08:15 -0300

    para fixar 3 e 4

--- Front-End/bloco-11-componentes-com-estado-eventos-e-formularios-com-react/dia-1-componentes-com-estado-e-eventos/fancy-buttons/src/App.js
+++ Front-End/bloco-11-componentes-com-estado-eventos-e-formularios-com-react/dia-1-componentes-com-estado-e-eventos/fancy-buttons/src/App.js
@@ -1,30 +1,40 @@
 import React from 'react'
 import './App.css';
 
-function handleClick1() {
-  console.log('click 1');
-}
-function handleClick2() {
-  console.log('click 2');
-}
-function handleClick3() {
-  console.log('click 3');
-}
+
 class App extends React.Component {
+  constructor() {
+    super()
+    this.handleClick1 = this.handleClick1.bind(this);
+    this.handleClick2 = this.handleClick2.bind(this);
+    this.handleClick3 = this.handleClick3.bind(this);
+  }
+  handleClick1() {
+    console.log(this);
+    console.log('click 1');
+  }
+  handleClick2() {
+    console.log(this);
+    console.log('click 2');
+  }
+  handleClick3() {
+    console.log(this);
+    console.log('click 3');
+  }
   render () {
     return (
     <>
       <label htmlFor='input'>
         <input name='input' type='password' />
-        <button onClick={ handleClick1 }>Butao</button>
+        <button onClick={ this.handleClick1 }>Butao</button>
       </label>
       <label htmlFor='input2'>
         <input name='input2' type='password' />
-        <button onClick={ handleClick2 }>Botao</button>
+        <button onClick={ this.handleClick2 }>Botao</button>
       </label>
       <label htmlFor='input3'>
         <input name='input4' type='password' />
-        <button onClick={ handleClick3 }>Botão</button>
+        <button onClick={ this.handleClick3 }>Botão</button>
       </label>
     </>
     );
