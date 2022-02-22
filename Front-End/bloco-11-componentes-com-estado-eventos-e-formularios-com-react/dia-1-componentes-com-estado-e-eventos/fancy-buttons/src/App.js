929e0057e1a417d88c03f092b69d40243f6d8048
MauroMarchesan88 <mauromarchesan88@gmail.com>
2022-02-22 11:25:54 -0300

    para fixar 5, 6 e 7

--- Front-End/bloco-11-componentes-com-estado-eventos-e-formularios-com-react/dia-1-componentes-com-estado-e-eventos/fancy-buttons/src/App.js
+++ Front-End/bloco-11-componentes-com-estado-eventos-e-formularios-com-react/dia-1-componentes-com-estado-e-eventos/fancy-buttons/src/App.js
@@ -5,36 +5,45 @@ import './App.css';
 class App extends React.Component {
   constructor() {
     super()
+    this.state = {
+      numeroDeCliques: 0,
+      numeroDeCliquesB: 0,
+      numeroDeCliquesC: 0
+    }
+    
     this.handleClick1 = this.handleClick1.bind(this);
     this.handleClick2 = this.handleClick2.bind(this);
     this.handleClick3 = this.handleClick3.bind(this);
   }
   handleClick1() {
-    console.log(this);
-    console.log('click 1');
+    this.setState((estadoAnterior, _props) => ({
+      numeroDeCliques: estadoAnterior.numeroDeCliques + 1
+    }))
   }
   handleClick2() {
-    console.log(this);
-    console.log('click 2');
+    this.setState((estadoAnterior, _props) => ({
+      numeroDeCliquesB: estadoAnterior.numeroDeCliquesB + 1
+    }))
   }
   handleClick3() {
-    console.log(this);
-    console.log('click 3');
+    this.setState((estadoAnterior, _props) => ({
+      numeroDeCliquesC: estadoAnterior.numeroDeCliquesC + 1
+    }))
   }
   render () {
     return (
     <>
       <label htmlFor='input'>
         <input name='input' type='password' />
-        <button onClick={ this.handleClick1 }>Butao</button>
+        <button onClick={ this.handleClick1 }>{ this.state.numeroDeCliques }</button>
       </label>
       <label htmlFor='input2'>
         <input name='input2' type='password' />
-        <button onClick={ this.handleClick2 }>Botao</button>
+        <button onClick={ this.handleClick2 }>{ this.state.numeroDeCliquesB }</button>
       </label>
       <label htmlFor='input3'>
         <input name='input4' type='password' />
-        <button onClick={ this.handleClick3 }>Botão</button>
+        <button onClick={ this.handleClick3 }>{ this.state.numeroDeCliquesC }</button>
       </label>
     </>
     );
