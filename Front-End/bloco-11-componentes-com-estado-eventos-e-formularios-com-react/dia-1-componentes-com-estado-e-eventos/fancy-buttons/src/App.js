cd39b1e55884220abf931cb34422c209bc48fcd8
MauroMarchesan88 <mauromarchesan88@gmail.com>
2022-02-22 13:51:49 -0300

    terminados exercicios para fixar 11.1

--- Front-End/bloco-11-componentes-com-estado-eventos-e-formularios-com-react/dia-1-componentes-com-estado-e-eventos/fancy-buttons/src/App.js
+++ Front-End/bloco-11-componentes-com-estado-eventos-e-formularios-com-react/dia-1-componentes-com-estado-e-eventos/fancy-buttons/src/App.js
@@ -15,35 +15,47 @@ class App extends React.Component {
     this.handleClick2 = this.handleClick2.bind(this);
     this.handleClick3 = this.handleClick3.bind(this);
   }
-  handleClick1() {
+  handleClick1(event) {
     this.setState((estadoAnterior, _props) => ({
       numeroDeCliques: estadoAnterior.numeroDeCliques + 1
     }))
+    this.state.numeroDeCliques % 2 === 0
+    ? event.target.className = 'blue'
+    : event.target.className = 'green';
+    console.log(event.target.className);
   }
-  handleClick2() {
+  handleClick2(event) {
     this.setState((estadoAnterior, _props) => ({
       numeroDeCliquesB: estadoAnterior.numeroDeCliquesB + 1
     }))
+    this.state.numeroDeCliquesB % 2 === 0
+    ? event.target.className = 'blue'
+    : event.target.className = 'green';
+    console.log(event.target.className);
   }
-  handleClick3() {
+  handleClick3(event) {
     this.setState((estadoAnterior, _props) => ({
       numeroDeCliquesC: estadoAnterior.numeroDeCliquesC + 1
     }))
+    this.state.numeroDeCliquesC % 2 === 0
+    ? event.target.className = 'blue'
+    : event.target.className = 'green';
+    console.log(event.target.className);
   }
   render () {
     return (
     <>
       <label htmlFor='input'>
         <input name='input' type='password' />
-        <button onClick={ this.handleClick1 }>{ this.state.numeroDeCliques }</button>
+        <button className='white' onClick={ this.handleClick1 }>{ this.state.numeroDeCliques }</button>
       </label>
       <label htmlFor='input2'>
         <input name='input2' type='password' />
-        <button onClick={ this.handleClick2 }>{ this.state.numeroDeCliquesB }</button>
+        <button className='white' onClick={ this.handleClick2 }>{ this.state.numeroDeCliquesB }</button>
       </label>
       <label htmlFor='input3'>
         <input name='input4' type='password' />
-        <button onClick={ this.handleClick3 }>{ this.state.numeroDeCliquesC }</button>
+        <button className='white' onClick={ this.handleClick3 }>{ this.state.numeroDeCliquesC }</button>
       </label>
     </>
     );
