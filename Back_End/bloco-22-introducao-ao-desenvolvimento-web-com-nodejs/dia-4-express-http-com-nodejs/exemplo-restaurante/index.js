0a710b274363eabb7cc79adaa6fc61fd02bb0831
Mauro <MauroMarchesan88@gmail.com>
2022-06-09 20:05:24 -0300

    Verificar search query nao retorna

--- Back_End/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-4-express-http-com-nodejs/exemplo-restaurante/index.js
+++ Back_End/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-4-express-http-com-nodejs/exemplo-restaurante/index.js
@@ -21,12 +21,40 @@ const drinks = [
 	{ id: 6, name: 'Água Mineral 500 ml', price: 5.0 },
 ];
 
-app.get('/recipes', function (req, res) {
-  res.json(recipes);
+app.get('/recipes', function (_req, res) {
+	res.json(recipes);
 });
 
-app.get('/drinks', function (req, res) {
-    res.json(drinks);
+app.get('/recipes/:id', function (req, res) {
+  const { id } = req.params;
+  const recipe = recipes.find((r) => r.id === Number(id));
+  if (!recipe) return res.status(404).json({message: 'Recipe not found!'});
+
+  res.status(200).json(recipe);
+});
+
+app.get('/recipes/search', function (req, res) {
+	const { name, maxPrice } = req.query;
+	const filteredRecipes = recipes.filter((r) => r.name.includes(name) && r.price < Number(maxPrice));
+	res.status(200).json(filteredRecipes);
+})
+
+app.get('/drinks', function (_req, res) {
+	res.json(drinks);
+});
+
+app.get('/drinks/:id', function (req, res) {
+  const { id } = req.params;
+  const drink = drinks.find((d) => d.id === Number(id));
+  if (!drink) return res.status(404).json({message: 'Recipe not found!'});
+
+  res.status(200).json(drink);
+});
+
+app.get('/drinks/search', function (req, res) {
+  const { name } = req.query;
+  const filteredDrinks = drinks.filter((r) => r.name.includes(name));
+  res.status(200).json(filteredDrinks);
 });
 
 app.listen(3001, () => {
