2c2bca8e5c7ed52bf92eb4032e44535302bdb9cd
Mauro <MauroMarchesan88@gmail.com>
2022-06-10 19:55:17 -0300

    Restaurante finalizado

--- Back_End/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-4-express-http-com-nodejs/exemplo-restaurante/index.js
+++ Back_End/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-4-express-http-com-nodejs/exemplo-restaurante/index.js
@@ -8,53 +8,53 @@ app.use(cors());
 
 const recipes = [
   { id: 1, name: 'Lasanha', price: 40.0, waitTime: 30 },
-  { id: 2, name: 'Macarrão a Bolonhesa', price: 35.0, waitTime: 25 },
-  { id: 3, name: 'Macarrão com molho branco', price: 35.0, waitTime: 25 },
+  { id: 2, name: 'Macarrao a Bolonhesa', price: 35.0, waitTime: 25 },
+  { id: 3, name: 'Macarrao com molho branco', price: 35.0, waitTime: 25 },
 ];
 
 const drinks = [
-	{ id: 1, name: 'Refrigerante Lata', price: 5.0 },
-	{ id: 2, name: 'Refrigerante 600ml', price: 8.0 },
-	{ id: 3, name: 'Suco 300ml', price: 4.0 },
-	{ id: 4, name: 'Suco 1l', price: 10.0 },
-	{ id: 5, name: 'Cerveja Lata', price: 4.5 },
-	{ id: 6, name: 'Água Mineral 500 ml', price: 5.0 },
+  { id: 1, name: 'Refrigerante Lata', price: 5.0 },
+  { id: 2, name: 'Refrigerante 600ml', price: 8.0 },
+  { id: 3, name: 'Suco 300ml', price: 4.0 },
+  { id: 4, name: 'Suco 1l', price: 10.0 },
+  { id: 5, name: 'Cerveja Lata', price: 4.5 },
+  { id: 6, name: 'Água Mineral 500 ml', price: 5.0 },
 ];
 
 app.get('/recipes', function (_req, res) {
-	res.json(recipes);
+  res.json(recipes);
 });
 
-app.get('/recipes/:id', function (req, res) {
-  const { id } = req.params;
-  const recipe = recipes.find((r) => r.id === Number(id));
-  if (!recipe) return res.status(404).json({message: 'Recipe not found!'});
-
-  res.status(200).json(recipe);
+app.get('/drinks', function (_req, res) {
+  res.json(drinks);
 });
 
 app.get('/recipes/search', function (req, res) {
-	const { name, maxPrice } = req.query;
-	const filteredRecipes = recipes.filter((r) => r.name.includes(name) && r.price < Number(maxPrice));
-	res.status(200).json(filteredRecipes);
+  const { name, maxPrice } = req.query;
+  const filteredRecipes = recipes.filter((r) => r.name.includes(name) && r.price < (Number(maxPrice)));
+  res.status(200).json(filteredRecipes);
 })
-
-app.get('/drinks', function (_req, res) {
-	res.json(drinks);
-});
-
-app.get('/drinks/:id', function (req, res) {
-  const { id } = req.params;
-  const drink = drinks.find((d) => d.id === Number(id));
-  if (!drink) return res.status(404).json({message: 'Recipe not found!'});
-
-  res.status(200).json(drink);
-});
 
 app.get('/drinks/search', function (req, res) {
   const { name } = req.query;
   const filteredDrinks = drinks.filter((r) => r.name.includes(name));
   res.status(200).json(filteredDrinks);
+});
+
+app.get('/recipes/:id', function (req, res) {
+  const { id } = req.params;
+  const recipe = recipes.find((r) => r.id === Number(id));
+  if (!recipe) return res.status(404).json({ message: 'Recipe not found!' });
+
+  res.status(200).json(recipe);
+});
+
+app.get('/drinks/:id', function (req, res) {
+  const { id } = req.params;
+  const drink = drinks.find((d) => d.id === Number(id));
+  if (!drink) return res.status(404).json({ message: 'Recipe not found!' });
+
+  res.status(200).json(drink);
 });
 
 app.listen(3001, () => {
