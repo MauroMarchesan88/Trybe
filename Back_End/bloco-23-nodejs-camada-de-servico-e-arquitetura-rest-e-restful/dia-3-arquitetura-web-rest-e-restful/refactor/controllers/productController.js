c6b63806c0b8ef5274f7f90472433bf5d2d199bf
Mauro <MauroMarchesan88@gmail.com>
2022-06-28 16:23:57 -0300

    Finalizados exercicios de fixação

--- Back_End/bloco-23-nodejs-camada-de-servico-e-arquitetura-rest-e-restful/dia-3-arquitetura-web-rest-e-restful/refactor/controllers/productController.js
+++ Back_End/bloco-23-nodejs-camada-de-servico-e-arquitetura-rest-e-restful/dia-3-arquitetura-web-rest-e-restful/refactor/controllers/productController.js
@@ -3,19 +3,19 @@ const ProductModel = require('../models/productModel');
 
 const router = express.Router();
 
-router.get('/list-products', async (req, res) => {
+router.get('/', async (_req, res) => {
     const products = await ProductModel.getAll();
 
     res.send(products);
 });
 
-router.get('/get-by-id/:id', async (req, res) => {
+router.get('/:id', async (req, res) => {
     const product = await ProductModel.getById(req.params.id);
 
     res.send(product);
 });
 
-router.post('/add-product', async (req, res) => {
+router.post('/', async (req, res) => {
     const { name, brand } = req.body;
 
     const newProduct = await ProductModel.add(name, brand);
@@ -23,13 +23,13 @@ router.post('/add-product', async (req, res) => {
     res.send(newProduct);
 });
 
-router.post('/delete-product/:id', async (req, res) => {
+router.delete('/:id', async (req, res) => {
     const products = await ProductModel.exclude(req.params.id);
 
     res.send(products);
 });
 
-router.post('/update-product/:id', async (req, res) => {
+router.put('/:id', async (req, res) => {
     const { name, brand } = req.body;
 
     const products = await ProductModel.update(req.params.id, name, brand);
